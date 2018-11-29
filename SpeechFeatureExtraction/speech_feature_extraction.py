832bc42ec5a64fa2c8b3f36cf13b6bb5629b85dc
RyoOzaki <ryo.ozaki@em.ci.ritsumei.ac.jp>
2018-11-28 21:18:31 -0800

    Update to use PSF and matching each mfcc which extracted by htk.

--- SpeechFeatureExtraction/speech_feature_extraction.py
+++ SpeechFeatureExtraction/speech_feature_extraction.py
@@ -1,6 +1,6 @@
 import numpy as np
-import scipy.signal
-from math import ceil, floor, log2
+from python_speech_features import mfcc, delta
+from math import ceil, floor
 
 def _load_label_list(files, sp=None):
     s = set()
@@ -35,7 +35,7 @@ class Extractor(object):
     _default_mfcc_cording_params={
         "winlen": 25.0 / 1000,
         "winstep": 10.0 / 1000,
-        "numcep": 12,
+        "numcep": 13,
         "nfilt": 20,
         "preemph": 0.97,
         "ceplifter": 22,
@@ -73,9 +73,7 @@ class Extractor(object):
         assert label_format in ("frame", "time")
         fs, data = self._loader.load(wav_file)
         N = data.shape[0]
-        window_frame = int(self._mfcc_cording_params["winlen"] * fs)
-        step_frame = int(self._mfcc_cording_params["winstep"] * fs)
-        mfcc = self._mfcc_cord(fs, data, fft_size=window_frame)
+        mfcc = self._mfcc_cord(data, fs)
         mfcc_d = delta(mfcc, self._delta_cording_param)
         mfcc_dd = delta(mfcc_d, self._delta_cording_param)
         M = mfcc.shape[0]
@@ -84,95 +82,9 @@ class Extractor(object):
             step_len = self._mfcc_cording_params["winstep"]
         phn = _label_cord(phn_file, self._phn, M, window_len, step_len)
         wrd = _label_cord(wrd_file, self._wrd, M, window_len, step_len)
-        return ((mfcc, mfcc_d, mfcc_dd), phn, wrd)
+        return (mfcc, mfcc_d, mfcc_dd), phn, wrd
 
-    def _mfcc_cord(self, fs, data, fft_size=-1):
-        kwargs  = Extractor._default_mfcc_cording_params.copy()
-        winstep = kwargs["winstep"]
-        winlen  = kwargs["winlen"]
-        numcep  = kwargs["numcep"]
-        preemph = kwargs["preemph"]
-        ceplifter = kwargs["ceplifter"]
-
-        data, nfft = split(data, fs, winlen, winstep, nfft=fft_size)
-        data = applyPreEmphasis(data, preemph)
-        data = applyWindow(data, np.hamming)
-        spec = np.abs(np.fft.fft(data, nfft, axis=1))[:, :nfft//2]
-        melfilterbank, _ = melFilterBank(fs, nfft, numcep+1)
-        melspec = np.log10(np.dot(spec, melfilterbank.T))
-        melceps = scipy.fftpack.realtransforms.dct(melspec, type=2, norm="ortho", axis=1)[:, 1:numcep+1]
-        mfcc = liftering(melceps, ceplifter)
-        return mfcc
-
-def hz2mel(f):
-    return 1127.01048 * np.log(f / 700.0 + 1.0)
-
-def mel2hz(m):
-    return 700.0 * (np.exp(m / 1127.01048) - 1.0)
-
-def applyPreEmphasis(signal, preemph):
-    return scipy.signal.lfilter([1.0, -preemph], 1, signal, axis=1)
-
-def applyWindow(signal, winfunc):
-    window = winfunc(signal.shape[1])
-    return window * signal
-
-def melFilterBank(samplerate, nfft, numChannels):
-    fmax = samplerate / 2
-    melmax = hz2mel(fmax)
-    nmax = nfft // 2
-    df = samplerate / nfft
-    dmel = melmax / (numChannels + 1)
-    melcenters = np.arange(1, numChannels + 1) * dmel
-    fcenters = mel2hz(melcenters)
-    indexcenter = np.round(fcenters / df)
-    indexstart = np.hstack(([0], indexcenter[0:numChannels - 1]))
-    indexstop = np.hstack((indexcenter[1:numChannels], [nmax]))
-
-    filterbank = np.zeros((numChannels, nmax))
-    for c in np.arange(0, numChannels):
-        increment = 1.0 / (indexcenter[c] - indexstart[c])
-        for i in np.arange(indexstart[c], indexcenter[c], dtype=int):
-            filterbank[c, i] = (i - indexstart[c]) * increment
-        decrement = 1.0 / (indexstop[c] - indexcenter[c])
-        for i in np.arange(indexcenter[c], indexstop[c], dtype=int):
-            filterbank[c, i] = 1.0 - ((i - indexcenter[c]) * decrement)
-
-    return filterbank, fcenters
-
-def liftering(cepstra, ceplifter):
-    if ceplifter > 0:
-        _, ncoeff = cepstra.shape
-        n = np.arange(ncoeff)
-        lift = 1 + (ceplifter/2.)*np.sin(np.pi*n/ceplifter)
-        return lift*cepstra
-    else:
-        # values of L <= 0, do nothing
-        return cepstra
-
-def split(raw_data, samplerate, winlen, winstep, nfft=-1):
-    win_frame = int(winlen * samplerate)
-    step_frame = int(winstep * samplerate)
-    if nfft < 0:
-        nfft = 2 ** ceil(log2(win_frame))
-        # nfft = win_frame
-    assert win_frame <= nfft
-    N = ceil((raw_data.shape[0] - win_frame) / step_frame) + 1
-    raw_data = np.concatenate((raw_data, np.zeros((N-1)*step_frame+win_frame)), axis=0)
-    indexies = np.arange(win_frame) + (np.arange(N) * step_frame).reshape((-1, 1))
-    offset = nfft - win_frame
-    left_padding = np.zeros((N, offset // 2))
-    right_padding = np.zeros((N, offset - (offset // 2)))
-    return np.concatenate((left_padding, raw_data[indexies], right_padding), axis=1), nfft
-
-def delta(data, N):
-    assert N >= 1
-    NUMFRAMES = data.shape[0]
-    delta = np.empty_like(data)
-    # denominator = 2 * sum([i**2 for i in range(1, N+1)])
-    denominator = N * (N+1) * (2*N+1) / 3.0
-    padded = np.pad(data, ((N, N), (0, 0)), mode='edge')   # padded version of feat
-    v = np.arange(-N, N+1)
-    for t in range(NUMFRAMES):
-        delta[t] = np.dot(v, padded[t : t+2*N+1]) / denominator   # [t : t+2*N+1] == [(N+t)-N : (N+t)+N+1]
-    return delta
+    def _mfcc_cord(self, data, fs):
+        kwargs = self._mfcc_cording_params
+        fft_size = int(kwargs["winlen"] * fs)
+        return mfcc(data, samplerate=fs, nfft=fft_size, **kwargs)[:, 1:]
